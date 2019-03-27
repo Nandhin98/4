984e0e94150cbdfad3023dd088cbabe8c31c51ec
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-03-27 16:07:18 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,12 @@
+import random
+        pie=int(input("total pie"))
+                                    n=int(input("no of persons"))
+                    l=[]
+            a=0
+        for i in range(1,n+1):
+       for i in range(pie):
+               ch=random.choice(l);
+               if ch==1:
+            else:
+            rint("player A=",a)
+                       print("player B=",b)
